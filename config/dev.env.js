d99e4f323789da24203a92a88fdbc9c6dc74e34a
mazhiyuan-s <m1468170820@163.com>
2020-03-10 15:19:30 +0800

    修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  // API_ROOT:'"http://linkingvision.cn:9080/"',
+  // WS_HOST_ROOT:'"linkingvision.cn:9080"'
   API_ROOT:'"http://localhost:8080/"',
   WS_HOST_ROOT:'"localhost:8080"'
 })
--- src/components/views/dashboard/GPU.vue
+++ src/components/views/dashboard/GPU.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="page-wrapper">
-        <div v-for="(a,index) in lent" :key="index">
-            <div :id="'container'+a"  style="width:100%;height: 400px;"></div>
+        <div class="container">
+            <div v-for="(a,index) in lent" :key="index" class="container_flex">
+                <div :id="'container'+a" style="width: 100%;height: 100%;"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -12,16 +14,53 @@ export default {
     name: "GPU",
     data() {
         return {
-            lent:3,
-            data1: [0,0,0,0,0,10,1000,50,63,72,4099,550,440,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            data2: [0,0,0,0,0,10,1000,50,63,72,4099,550,440,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            data3: [0,0,0,0,0,10,1000,50,63,72,4099,550,440,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            echartsdata:[],
+            lent:12,
+            data:[
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+
+                {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                data1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+            ],
+            Gpudata:[],
         }
     },
     mounted(){
-        this.GPU();
-        // this.Gpufo();
+        this.Gpufo();
+        this.timerRunInfo = setInterval(() => {
+            this.Gpufo();
+        }, 5000);
+        
     },
     methods:{
         Gpufo(){
@@ -36,14 +75,93 @@ export default {
             this.$http.get(url).then(result => {
                 if (result.status == 200) {
                     var data = result.data;
-                    this.lengt=data.intel.length+data.nvidia.length
-                    console.log(data.intel.length,data.nvidia.length);
+                    /**var data={
+                        "intel": [],
+                        "nvidia": [
+                            {
+                            "nIndex": 0,
+                            "strName": "GeForce GTX 1660 Ti"
+                            },
+                            {
+                            "nIndex": 1,
+                            "strName": "GeForce GTX 1660"
+                            },
+                            {
+                            "nIndex": 1,
+                            "strName": "GeForce GTX 1660"
+                            }
+                        ]
+                    }**/
+                    this.Gpudata=data;
+                    // var lengthin=data.intel.length;
+                    // var lengthnv=data.nvidia.length;
+                    this.lent=data.nvidia.length;
+                    this. Gpuul(data);
                 }
             })
         },
-        GPU() {
+        Gpuul(datafo){
+            let _this = this;
+            var root = process.env.API_ROOT;
+            if (root == undefined) {
+                root =window.location.protocol + "//" +window.location.host +window.location.pathname;
+            }
+
+            var url =root + "/api/v1/GetGPULoad?session=" + this.$store.state.token;
+            this.$http.get(url).then(result => {
+                if (result.status == 200) {
+                    var data = result.data;
+                    /**var data={
+                        "bSupportIntel": false,
+                        "bSupportNVIDIA": true,
+                        "intel": [],
+                        "nvidia": [
+                            {
+                            "nIndex": 0,
+                            "nEncodeUsage": 3,
+                            "nDecodeUsage": 2
+                            },
+                            {
+                            "nIndex": 1,
+                            "nEncodeUsage": 30,
+                            "nDecodeUsage": 20
+                            },
+                            {
+                            "nIndex": 1,
+                            "nEncodeUsage": 80,
+                            "nDecodeUsage": 60
+                            }
+                        ]
+                    }**/
+                    var nvidiadata=[];
+                    // return false;
+                    for(var i=0;i<data.nvidia.length;i++){
+                        if(datafo.nvidia[i].nIndex==data.nvidia[i].nIndex){
+                            var dataroot={
+                                nIndex:data.nvidia[i].nIndex,
+                                strName:datafo.nvidia[i].strName,
+                                nEncodeUsage:data.nvidia[i].nEncodeUsage,
+                                nDecodeUsage:data.nvidia[i].nDecodeUsage,
+                            }
+                            nvidiadata.push(dataroot)
+                        }
+                    }
+                    var lengthnv=nvidiadata.length;
+                    for(var i=0;i<nvidiadata.length;i++){
+                        this.data[i].data.push(nvidiadata[i].nEncodeUsage);
+                        this.data[i].data.splice(0, 1);
+                        this.data[i].data1.push(nvidiadata[i].nDecodeUsage);
+                        this.data[i].data1.splice(0, 1);
+                        console.log("******",i)
+                        this.GPUnv(lengthnv,nvidiadata,i);
+                    }
+                }
+            })
+        },
+        GPUnv(lengthnv,nvidiadata,l){
             var base = +new Date();
             var date = [];
+            var _this=this;
             var oneSecond = 1000;
             for (var i = 1; i <= 64; i++) {
                 var now = new Date((base += oneSecond));
@@ -51,109 +169,117 @@ export default {
                     [('0' + now.getSeconds()).slice(-2) + 's']
                 )
             }
-            var lent=this.lent;
-            for(var i=1;i<=lent;i++){
-                var pieId = document.getElementById('container'+i);
-                if (!pieId){
-                    return false;
-                }
-                var myChart = echarts.init(pieId)
-                var echdata="this.data"+i
-                typeof echdata
-                console.log(i,echdata)
+            var pieId = document.getElementById('container'+(l+1));
+            if (!pieId){
                 return false;
-                // 绘制图表
-                myChart.setOption({
-                    tooltip: {
-                        trigger: 'axis',
-                        position: function(pt) {
-                            return [pt[0], '10%']
-                        },
-                        formatter:"{b0}<br />{a0}:{c0}Kbps<br />{a1}:{c1}Kbps"
-                    },
-                    title: {
-                        left: 'center',
-                        text: this.title_text,
-                        show:false
-                    },
-                    legend: {
-                            data:[ this.network_in,this.network_out],
-                            icon:'rect',
-                            itemWidth: 35,
-                            itemHeight: 2,
-                        },
-                    xAxis: {
-                        type: 'category',
-                        boundaryGap: false,
-                        data: date,
-                        axisLine: {
-                            show: false
-                        },
-                        axisTick: {
-                            show: false
-                        },
-                        splitLine: { //网格线
-                            "show": false
-                        }
-                    },
-                    yAxis: {
-                        type: 'value',
-                        axisLine: {
-                            show: false
-                        },
-                        splitLine: { //网格线
-                            "show": false
-                        },
-                        axisTick: {
-                            show: false
-                        }
-                    },
-                    series: [{
-                        name: this.network_in,
-                        type: 'line',
-                        smooth: true,
-                        symbol: 'none',
-                        sampling: 'average',
-                        itemStyle: {
-                            color: '#0BDAB4'
-                        },
-                        areaStyle: {
-                            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                                    offset: 0,
-                                    color: 'rgba(11,218,180,0.5)'
-                                },
-                                {
-                                    offset: 1,
-                                    color: 'rgba(255,255,255,.2)'
-                                }
-                            ])
-                        },
-                        data: this.data
-                    }, {
-                        name: this.network_out,
-                        type: 'line',
-                        smooth: true,
-                        symbol: 'none',
-                        sampling: 'average',
-                        itemStyle: {
-                            color: '#FF9C66'
-                        },
-                        areaStyle: {
-                            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                                    offset: 0,
-                                    color: 'rgba(255,156,102,0.5)'
-                                },
-                                {
-                                    offset: 1,
-                                    color: 'rgba(255,255,255,.2)'
-                                }
-                            ])
-                        },
-                        data: echdata
-                    }, ]
-                })
             }
+            var myChart = echarts.init(pieId)
+            // return false;
+            // 绘制图表
+            myChart.setOption({
+                tooltip: {
+                    trigger: 'axis',
+                    position: function(pt) {
+                        return [pt[0], '10%']
+                    },
+                    formatter:"{b0}<br />{a0}:{c0}Kbps<br />{a1}:{c1}Kbps"
+                },
+                title: {
+                    left: 'center',
+                    text: this.title_text,
+                    show:false
+                },
+                legend: {
+                        data:[ this.network_in,this.network_out],
+                        icon:'rect',
+                        itemWidth: 35,
+                        itemHeight: 2,
+                    },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: date,
+                    axisLine: {
+                        show: false
+                    },
+                    axisTick: {
+                        show: false
+                    },
+                    splitLine: { //网格线
+                        "show": false
+                    }
+                },
+                yAxis: {
+                    type: 'value',
+                    axisLine: {
+                        show: false
+                    },
+                    splitLine: { //网格线
+                        "show": false
+                    },
+                    axisTick: {
+                        show: false
+                    }
+                },
+                series: [{
+                    name: this.network_in,
+                    type: 'line',
+                    smooth: true,
+                    symbol: 'none',
+                    sampling: 'average',
+                    itemStyle: {
+                        color: '#0BDAB4'
+                    },
+                    areaStyle: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                offset: 0,
+                                color: 'rgba(11,218,180,0.5)'
+                            },
+                            {
+                                offset: 1,
+                                color: 'rgba(255,255,255,.2)'
+                            }
+                        ])
+                    },
+                    data: this.data[l].data
+                }, {
+                    name: this.network_out,
+                    type: 'line',
+                    smooth: true,
+                    symbol: 'none',
+                    sampling: 'average',
+                    itemStyle: {
+                        color: '#FF9C66'
+                    },
+                    areaStyle: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                offset: 0,
+                                color: 'rgba(255,156,102,0.5)'
+                            },
+                            {
+                                offset: 1,
+                                color: 'rgba(255,255,255,.2)'
+                            }
+                        ])
+                    },
+                    data: this.data[l].data
+                }, ]
+            })
         },
     },
 }
 </script>
+<style scoped>
+.container{
+    width: 100%;
+    background-color: #FFFFFF;
+    min-height: 900px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+.container .container_flex{
+    width: 50%;
+    height: 400px;
+}
+</style>
